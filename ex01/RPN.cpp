18cddab23061fef427b84de94fe8e072952457ac
Najat Khoudrouf <nkhoudro@student.1337.ma>
2024-03-06 21:48:02 +0100

    error in input

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -10,15 +10,26 @@ RPN::~RPN()
 void RPN::calculate()
 {
    size_t pos = 0;
+   (void)_a;
+    (void)_b;
+    (void)_result;
     while ((pos = _expression.find(" " , pos)) != std::string::npos)
     {
-        std::cout << _expression.substr(0, pos) << std::endl;
-        if (_expression[pos + 1] == '+' || _expression[pos + 1] == '-' || _expression[pos + 1] == '*' || _expression[pos + 1] == '/' || _expression[pos + 1] == '%')
+        if (( _expression[pos + 1] == '+' || _expression[pos + 1] == '-' || _expression[pos + 1] == '*' || _expression[pos + 1] == '/' ))
         {
+            if (stack.empty())
+                throw std::runtime_error("Invalid expression");
             _b = stack.top();
             stack.pop();
-            _a = stack.top();
-            stack.pop();
+            if (stack.empty())
+            {
+                _a = _result;
+            }
+            else
+            {
+                _a = stack.top();
+                stack.pop();
+            }
             if (_expression[pos + 1] == '+')
                 _result = _a + _b;
             else if (_expression[pos + 1] == '-')
@@ -27,12 +38,12 @@ void RPN::calculate()
                 _result = _a * _b;
             else if (_expression[pos + 1] == '/')
                 _result = _a / _b;
-            else if (_expression[pos + 1] == '%')
-                _result = _a % _b;
             stack.push(_result);
         }
         else
             stack.push(std::stoi(_expression.substr(0, pos)));
+        pos++;
         // stack.push(std::stoi(_expression.substr(0, pos)));
     }
+    std::cout << "Result: " << _result << std::endl;
 }
